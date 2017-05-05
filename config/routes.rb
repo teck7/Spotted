50ef44339584de0fed6d387181172ef2d004c4cb
tek7 <teckyap@hotmail.com.au>
2017-05-05 14:22:55 +1000

    add_devise_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'spotted_pages#home'
-  # get 'spotted_pages/home'
+  # Devise routes for User Model
+  devise_for :users
 
+  # Static pages routes
+  root 'spotted_pages#home'
   get 'faq', to: 'spotted_pages#faq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
