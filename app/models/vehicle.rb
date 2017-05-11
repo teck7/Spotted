8b0d4df041a4e7771ebcf4a076a2ee805e3c2a75
tek7 <teckyap@hotmail.com.au>
2017-05-11 10:19:19 +1000

    re-configure_re-validate_all_current_feature

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,9 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
+
+  def self.search(search)
+    where("car_make ILIKE ? OR car_model ILIKE ? OR car_built ILIKE ? OR car_color ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
+
 end
